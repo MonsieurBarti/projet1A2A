6fa9fa0ccc8a8de0d372887168f99150f2ddf7f7
Pierre_LE_CORFF <21804628@etu.unicaen.fr>
2019-06-13 09:26:57 +0200

    Jui bo et j'ai fix de nombreux problèmes alors que pierre a rien fait ce pd

---
+++ routes/users.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+//Bring in Article Model
+let User = require('../models/user');
+
+
+//Register Form
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+
+//Register Process
+router.post('/register', (req, res) => {
+  const name = req.body.name;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    res.render('register', {
+      errors:errors
+    });
+  } else {
+    let newUser = new User({
+      name:name,
+      username:username,
+      email:email,
+      password:password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) {
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save((err) => {
+          if (err) {
+            console.log(err);
+            return;
+          } else {
+            req.flash('success', 'You are now registered and can log in');
+            res.redirect('/users/login');
+          }
+        })
+      });
+    });
+  }
+});
+
+//Log In Form
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+//Log In Process
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect:'/',
+    failureRedirect:'/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+//Log Out
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', 'You are logged out');
+  res.redirect('/users/login');
+})
+module.exports = router;
